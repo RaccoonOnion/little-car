4977e2dc0cda3960fdba0a92e8fac178170e7d4c
Yunxiang Yan <104146444+RaccoonOnion@users.noreply.github.com>
2022-12-09 22:35:24 +0800

    manual version 1.0

--- source/dev_top.v
+++ source/dev_top.v
@@ -21,31 +21,39 @@
 
 
 module SimulatedDevice(
-    input sys_clk, //bind to P17 pin (100MHz system clock)
-    input rx, //bind to N5 pin
-    output tx, //bind to T4 pin
-    
-    input rst_n, // reset
-    input power_on_signal, power_off_signal,
-    input manual_driving_signal,
-    input throttle_signal, clutch_signal, // brake, reverse TODO!!
-    
-    output reg [3:0] state_led,
-    
+    // 1. original (11)
+    input sys_clk, // (100MHz system clock)
+    input rx,
     input turn_left_signal,
     input turn_right_signal,
-    input move_forward_signal,
-    input move_backward_signal,
     input place_barrier_signal,
     input destroy_barrier_signal,
+    
+    output tx,
     output front_detector,
     output back_detector,
     output left_detector,
-    output right_detector
+    output right_detector,
+    // -------------------------------------------------------------
+    
+    // 2. new (11 + 4)
+    input rst, // reset
+    input power_on_signal, 
+    input power_off_signal,
+    input manual_driving_signal,
+    input throttle_signal,
+    input clutch_signal,
+    input brake_signal,
+    input reverse_signal,
+    
+    output reg [3:0] state_led,
+    output reg left_turn_led,
+    output reg right_turn_led,
+    output reg reverse_led
     );
 
     reg[3:0] state, next_state;
-    reg turn_left, turn_right, move_forward, move_backward, place_barrier, destroy_barrier; // remove some TODO!!
+    reg turn_left, turn_right, move_forward, move_backward, place_barrier, destroy_barrier;
     wire power_on_1sec; // power on for 1s
     wire clk_ms, clk_20ms, clk_16x, clk_x; // clock division
 
@@ -65,51 +73,38 @@ module SimulatedDevice(
         end
         not_starting: // not starting state
         begin
-            if(clutch_signal)
-            begin
-                if(throttle_signal) next_state = starting;
-                else next_state = not_starting;
-            end
-            else 
-            begin
-                if(throttle_signal) next_state = power_off;
-                else next_state = not_starting;
-            end
+            casex ({throttle_signal, brake_signal, clutch_signal})
+                3'b101: next_state = starting;
+                3'b1x0: next_state = power_off;
+                default: next_state = not_starting;
+            endcase
         end
+        
         starting: // starting state
         begin
-            if(throttle_signal)
-            begin
-                if(~clutch_signal) next_state = moving;
-                else next_state = starting;
-            end 
-            else next_state = starting;  
+            casex ({throttle_signal, brake_signal, clutch_signal})
+                3'bx1x: next_state = not_starting;
+                3'b100: next_state = moving;
+                default: next_state = starting;
+            endcase
         end
         moving: // moving state
         begin
-            if(~throttle_signal || clutch_signal) next_state = starting;
-            else next_state = moving;
+            casex ({throttle_signal, brake_signal, reverse_signal, clutch_signal})
+                4'bxx10: next_state = power_off;
+                4'bx100, 4'bx101, 4'bx111: next_state = not_starting;
+                4'bx0x1, 4'b0000: next_state = starting;
+                default: next_state = moving;
+            endcase
         end
 
         default: next_state = next_state; // TODO!!
-
         endcase
     end
-    
-    // always @(posedge sys_clk, negedge rst_n) // State Register
-    // begin 
-    //     if(~rst_n)
-    //     begin
-    //         state <= power_off;
-    //         // output set to zero TODO!!
-    //     end
-    //     else
-    //         state <= next_state;
-    // end
 
-    always @(posedge sys_clk, negedge rst_n) // State Register
+    always @(posedge sys_clk, negedge rst) // State Register
     begin
-        if(~rst_n)
+        if(~rst)
         begin
             state <= power_off;
             // output set to zero TODO!!
@@ -128,9 +123,18 @@ module SimulatedDevice(
             power_on: {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= 8'b0;
             power_off: {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= 8'b0;
             not_starting: {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= 8'b0;
-            starting: {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= 8'b0;
-            // reverse function TODO!!
-            moving: {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {turn_left_signal,turn_right_signal,1'b1,1'b0,place_barrier_signal,destroy_barrier_signal};    
+            starting: 
+            begin
+            if (reverse_signal)
+                {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {turn_left_signal,turn_right_signal,1'b0,1'b1,place_barrier_signal,destroy_barrier_signal}; 
+            else
+                {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= 8'b0;
+            end
+            
+            moving: 
+            begin
+            {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {turn_left_signal,turn_right_signal,1'b1,1'b0,place_barrier_signal,destroy_barrier_signal}; 
+            end   
             endcase
         end
     end
@@ -138,14 +142,10 @@ module SimulatedDevice(
     always@(*) // Output Combinational Logic
     begin
         state_led = state;
-        // case(state)
-        // power_off: state_led <= power_off;
-        // power_on: state_led <= power_on;
-        // not_starting: state_led <= not_starting;
-        // starting: state_led <= starting;
-        // moving: state_led <= moving;
-        // default: ;
-        // endcase
+        left_turn_led = turn_left_signal;
+        right_turn_led = turn_right_signal;
+        reverse_led = reverse_signal;
+        // Milage led output TODO!!
     end
     
     wire [7:0] in = {2'b10, destroy_barrier, place_barrier, turn_right, turn_left, move_backward, move_forward};
@@ -156,7 +156,7 @@ module SimulatedDevice(
     assign left_detector = rec[2];
     assign right_detector = rec[3];
     
-    power_on_judge poj(clk_20ms, rst_n, power_on_signal, power_on_1sec); // power on 1 sec
+    power_on_judge poj(clk_20ms, rst, power_on_signal, power_on_1sec); // power on 1 sec
     
     uart_top md(.clk(sys_clk), .rst(0), .data_in(in), .data_rec(rec), .rxd(rx), .txd(tx)); // uart top
 
