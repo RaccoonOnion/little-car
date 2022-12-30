12166a73d267a6c0b7063cbb933e4d56de13c0df
Yunxiang Yan <104146444+RaccoonOnion@users.noreply.github.com>
2022-12-30 20:33:11 +0800

    update dev_top

--- source/dev_top.v
+++ source/dev_top.v
@@ -41,10 +41,12 @@ module SimulatedDevice(
     input power_on_signal, 
     input power_off_signal,
     input manual_driving_signal,
+    input semi_manual_driving_signal,
     input throttle_signal,
     input clutch_signal,
     input brake_signal,
     input reverse_signal,
+    input move_forward_signal,
     
     output reg [3:0] state_led,
     output reg left_turn_led,
@@ -52,7 +54,8 @@ module SimulatedDevice(
     output reg reverse_led,
     output [7:0] seg_en,
     output [7:0] seg_out0,
-    output [7:0] seg_out1
+    output [7:0] seg_out1,
+    output fork_here
     );
 
     reg[3:0] state, next_state;
@@ -61,8 +64,10 @@ module SimulatedDevice(
     wire reverse_change; // switch reverse without clutch
     wire clk_ms, clk_20ms, clk_100ms, clk_s; // clock division
     wire flash_led; // flashing led light
+    wire detect_fork; // detecting fork road
+    wire finish_turning; // finish turning signal 
     
-    parameter power_off = 4'b0000, power_on = 4'b0001, not_starting = 4'b0010, starting = 4'b0011, moving = 4'b0100; // more states TODO!!
+    parameter power_off = 4'b0000, power_on = 4'b0001, not_starting = 4'b0010, starting = 4'b0011, moving = 4'b0100, sauto_moving = 4'b0101, sauto_waiting = 4'b0110, sauto_turning = 4'b0111;// more states TODO!!
     
     always @(*) begin //  Next State Combinational Logic TODO!! add power_off
         case(state)
@@ -74,6 +79,7 @@ module SimulatedDevice(
         begin
             if(power_off_signal) next_state = power_off;
             else if(manual_driving_signal) next_state = not_starting;
+            else if(semi_manual_driving_signal) next_state = sauto_moving;
             else next_state = power_on;
         end
         not_starting: // not starting state
@@ -106,7 +112,42 @@ module SimulatedDevice(
             endcase
             end
         end
-        
+        sauto_moving:
+        begin
+            if (fork_here)
+            begin
+                next_state = sauto_waiting;
+            end
+            else
+            begin
+                next_state = sauto_moving;
+            end
+        end
+        sauto_waiting:
+        begin
+            if (move_forward_signal)
+            begin
+                next_state = sauto_moving;
+            end
+            else if(turn_left_signal || turn_right_signal)
+            begin
+                next_state = sauto_turning;
+            end
+            else
+            begin
+                next_state = sauto_waiting;
+            end
+        end
+        sauto_turning:
+        begin
+            if (finish_turning)
+            begin
+                 next_state = sauto_moving;
+            end
+            else
+            begin
+            end
+        end
         default: next_state = next_state; // TODO!!
         endcase
     end
@@ -142,6 +183,22 @@ module SimulatedDevice(
             else
                 {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {turn_left_signal,turn_right_signal,1'b0,1'b1,place_barrier_signal,destroy_barrier_signal}; 
             end
+            
+            sauto_moving:
+            begin
+                {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {1'b0,1'b0,1'b1,1'b0,1'b0,1'b0};
+            end
+            
+            sauto_waiting:
+            begin
+                {turn_left,turn_right,move_forward,move_backward,place_barrier,destroy_barrier} <= {1'b0,1'b0,1'b0,1'b0,1'b0,1'b0};
+            end
+            
+            sauto_turning:
+            begin
+            // write here
+            end
+            
             endcase
         end
     end
@@ -181,6 +238,21 @@ module SimulatedDevice(
             end         
             reverse_led = reverse_signal;
         end
+        sauto_moving:
+        begin
+            left_turn_led = 1'b0;
+            right_turn_led = 1'b0;
+            reverse_led = 1'b0;
+        end
+        sauto_waiting:
+        begin
+            left_turn_led = flash_led;
+            right_turn_led = flash_led;
+            reverse_led = flash_led;
+        end
+        sauto_turning:
+        begin
+        end
         default:
         begin
             left_turn_led = 1'b0;  
@@ -199,12 +271,13 @@ module SimulatedDevice(
     assign left_detector = rec[1];
     assign right_detector = rec[2];
     assign back_detector = rec[3];
-    
+
     seg s(clk_ms, clk_100ms, rst, state, seg_en, seg_out0, seg_out1); //seg 
     clk_div cd( .clk(sys_clk), .rst_n(rst), .clk_ms(clk_ms), .clk_20ms(clk_20ms), .clk_100ms(clk_100ms), .clk_s(clk_s)); // clock division
     power_on_judge poj(clk_20ms, rst, power_on_signal, power_on_1sec); // power on 1 sec
     edge_detector ed1(.clk(sys_clk), .rst_n(rst), .signal(reverse_signal), .double_edge_detect(reverse_change) );// detect reverse change
     flash_led fled1(.clk(clk_ms), .rst_n(rst), .flash_led(flash_led)); // flash_led
+    detect_fork df1(clk_100ms, rst, {front_detector,back_detector,left_detector, right_detector}, fork_here);
     uart_top md(.clk(sys_clk), .rst(0), .data_in(in), .data_rec(rec), .rxd(rx), .txd(tx)); // uart top
    
 endmodule
